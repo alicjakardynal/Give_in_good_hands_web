638a7b21c64f70016ee803d0e038aae32d6cc65e
alicjakardynal <k.alicja66@gmail.com>
2019-12-15 19:08:41 +0100

    startWithFetch

--- src/components/HomeWhoWeHelp.js
+++ src/components/HomeWhoWeHelp.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 
 class HomeWhoWeHelp extends Component{
+    state={
+        funadationView: true,
+        organizationsView: false,
+        localsView: false,
+    }
+
+
+    componentDidMount() {
+        changingView()
+    }
+
+    changingView = () => {
+       fetch(`....`,
+                {
+                    method: 'GET'
+                })
+                .then(resp => {
+                     return resp.json();
+            
+                })
+            }
+
+
     render() {
         return (
             <section>
                 <h2>Komy pomagamy?</h2>
-                <p className="decoaration"></p>
+                <p className="decoration"></p>
                 <div> te trzy do wybierania opcje</div>
-                <div>cos co sie podmienia</div>
-                <div> text z jasona</div>
+                <Fundation funadationView={this.state.funadationView}/>
+                <Organization organizationsView={this.state.organizationsView}/>
+                <Local  localsView={this.state.localsView}/>
+                <div> tutaj przyciski i na onClicku każdego będzie zmieniał false na true</div>
                 
             </section>
         )
