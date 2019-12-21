5a60e76c4ae0fede9a49f67049d47db5081b5608
alicjakardynal <k.alicja66@gmail.com>
2019-12-21 15:54:34 +0100

    forms

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -1,35 +1,122 @@
 import React, { Component } from "react";
 
-class HomeForm extends Component{
-    render() {
-        return (
-            <section className="form_section" id="contact">
-                <div className="form">
-                    <h2>Skontaktuj się z nami</h2>
-                    <p className="decoration"></p>
-                <form>
-                    <div>
-                    <label>Wpisz swoje imię:
-                        <input type="text"  placeholder="Krzysztof"name="username"/>
-                    </label>
-                    <label>Wpisz swoj email:
-                        <input type="email" placeholder="xyz@.pl" name="useremail"/>
-                    </label>
-                    </div>
-                    <label className="text_area">Wpisz swoją wiadomość:
-                        <textarea placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-                    </label>
-                    <div className="send_button">
-                    <input className="send" type="submit" value="Wyślij"/>
-                    </div>
-                </form>
-                </div>
-            </section>
-        )
+class HomeForm extends Component {
+  state = {
+    name: "",
+    wrongName: "",
+    email: "",
+    wrongEmail: "",
+    message: "",
+    wrongMessage: "",
+    sended:"",
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  sendLoginData = () => {
+      console.log("wysyla sie ");
+      this.setState({
+          sended:"Wiadomość została wysłana.Wkrótce się skontaktujemy"
+      });
+      fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+        method : 'POST',
+        body: JSON.stringify( {
+            name:this.state.name,
+            email:this.state.email,
+            message:this.state.message,
+        } )
+      });
+
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (/\s/.test(this.state.name)) {
+      this.setState({
+        wrongName: "Podane imię jest nieprawidłowe!"
+      });
     }
+    if (!this.validateEmail(this.state.email)) {
+      this.setState({
+        wrongEmail: "Podany email jest nieprawidłowy!"
+      });
+    }
+    if (this.state.message.length < 120) {
+      this.setState({
+        wrongMessage: "Wiadomość musi mieć conajmniej 120 znaków"
+      });
+    }
+    this.sendLoginData();
+    //w przeciwnym nie ma tych to wysylam do fetcha
+  };
+
+  validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  render() {
+    return (
+      <section className="form_section" id="contact">
+        <div className="form">
+          <h2>Skontaktuj się z nami</h2>
+          <p className="decoration"></p>
+          <p className="sended">{this.state.sended}</p>
+          <form onSubmit={this.handleSubmit}>
+            <div className="name_mail_area">
+              <div>
+                <label>
+                  Wpisz swoje imię:
+                  <input
+                    type="text"
+                    value={this.state.name}
+                    placeholder="Krzysztof"
+                    name="name"
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <p className="error">{this.state.wrongName}</p>
+              </div>
+              <div>
+              <label>
+                Wpisz swoj email:
+                <input
+                  type="email"
+                  value={this.state.email}
+                  placeholder="xyz@.pl"
+                  name="email"
+                  onChange={this.handleChange}
+                />
+              </label>
+              <p className="error">{this.state.wrongEmail}</p>
+              </div>
+            </div>
+            <div className="text_area_div">
+              <label className="text_area">
+                Wpisz swoją wiadomość:
+                <textarea
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                  value={this.state.message}
+                  name="message"
+                  onChange={this.handleChange}
+                />
+              </label>
+              <p className="error">{this.state.wrongMessage}</p>
+            </div>
+            <div className="send_button">
+              <input className="send" type="submit" value="Wyślij" />
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  }
 }
 
-
-export default HomeForm
+export default HomeForm;
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,25 +2,76 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 class SignIn extends Component {
+  state = {
+    email: "",
+    wrongEmail: "",
+    password: "",
+    wrongPassword: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (!this.validateEmail(this.state.email)) {
+      this.setState({
+        wrongEmail: "Podany email jest nieprawidłowy!"
+      });
+    }
+    if (this.state.password.length < 6) {
+      this.setState({
+        wrongPassword: "Podane hasło jest nieprawidłowe!"
+      });
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
   render() {
     return (
       <section className="signing">
         <h2>Zaloguj się</h2>
         <p className="decoration"></p>
-        <form>
-          <label>
-            Email
-            <input type="email" name="useremail" />
-          </label>
-          <label>
-            Hasło
-            <input type="password" name="userepassword" />
-          </label>
-        </form>
-        <div>
-          <NavLink className="link sign"exact to="/register">Załóż konto</NavLink>
-          <NavLink className="link sign"exact to="/signin">Zaloguj się</NavLink>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              Email
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </label>
+            <p className="error">{this.state.wrongEmail}</p>
+          </div>
+          <div>
+            <label>
+              Hasło
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </label>
+            <p className="error">{this.state.wrongPassword}</p>
+          </div>
+       
+        <div className="buttons">
+          <NavLink className="link sign" exact to="/register">
+            Załóż konto
+          </NavLink>
+         <input className="sign" type="submit" value="Zaloguj się" />
         </div>
+         </form>
       </section>
     );
   }
