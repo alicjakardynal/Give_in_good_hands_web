29d51eccac0ce4b36a59df360ba6fae33196db69
alicjakardynal <k.alicja66@gmail.com>
2020-02-03 18:00:22 +0100

    Updates

---
+++ precache-manifest.8b605dd693384c105a3d39210744d964.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e1e908bd7f8df355b166ed0af1c565",
+    "url": "/Give_in_good_hands_web/index.html"
+  },
+  {
+    "revision": "cdca33698b50ba40d0ed",
+    "url": "/Give_in_good_hands_web/static/css/main.6c7c3b48.chunk.css"
+  },
+  {
+    "revision": "c1f808f4c5cfca1cf14e",
+    "url": "/Give_in_good_hands_web/static/js/2.7f8b62db.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Give_in_good_hands_web/static/js/2.7f8b62db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdca33698b50ba40d0ed",
+    "url": "/Give_in_good_hands_web/static/js/main.c1799734.chunk.js"
+  },
+  {
+    "revision": "92b714dd083d9117e0a9",
+    "url": "/Give_in_good_hands_web/static/js/runtime-main.ea59813f.js"
+  },
+  {
+    "revision": "7db36abd712c228ea96e6206440a9eb3",
+    "url": "/Give_in_good_hands_web/static/media/3 Columns Background.7db36abd.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Give_in_good_hands_web/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Give_in_good_hands_web/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Give_in_good_hands_web/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "ef2519d20c70fe42a96e8c744772c6ef",
+    "url": "/Give_in_good_hands_web/static/media/Hero Image.ef2519d2.png"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Give_in_good_hands_web/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Give_in_good_hands_web/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Give_in_good_hands_web/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Give_in_good_hands_web/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Give_in_good_hands_web/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Give_in_good_hands_web/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Give_in_good_hands_web/static/media/Signature.e54cfcfc.svg"
+  }
+]);
